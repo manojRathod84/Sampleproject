46fc6c59426b3ff150131081b77b88a1082c4c9d
manojRathod84 <45167991+manojRathod84@users.noreply.github.com>
2019-12-24 14:53:22 +0530

    final commit

--- demo/lib/floating_menu.dart
+++ demo/lib/floating_menu.dart
@@ -1,33 +1,7 @@
 import 'package:flutter/material.dart';
 import 'model.dart';
 
-class DisplayListView extends StatefulWidget {
-  @override
-  _DisplayListViewState createState() => _DisplayListViewState();
-}
 
-class _DisplayListViewState extends State {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder( 
-      itemCount: listViewData.length,
-      itemBuilder: (context, int i) => Column(
-        children: [
-          new ListTile(
-            leading: new CircleAvatar(child: Text(listViewData[i].avatarURL)),
-            title: new Text(listViewData[i].title),
-            onTap: (){},
-            onLongPress: (){
-              print(
-                Text("Long Pressed"),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class ListViewExample extends StatelessWidget {
   @override
@@ -44,3 +18,4 @@ class ListViewExample extends StatelessWidget {
     );
   }
 }
+
