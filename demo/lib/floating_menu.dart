38b4bb02d0b5925b6c39578d1c0415264c928a3b
manojRathod84 <45167991+manojRathod84@users.noreply.github.com>
2019-12-24 14:29:41 +0530

    Final Commit

--- demo/lib/floating_menu.dart
+++ demo/lib/floating_menu.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
 import 'model.dart';
 
+class DisplayListView extends StatefulWidget {
+  @override
+  _DisplayListViewState createState() => _DisplayListViewState();
+}
+
+class _DisplayListViewState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder( 
+      itemCount: listViewData.length,
+      itemBuilder: (context, int i) => Column(
+        children: [
+          new ListTile(
+            leading: new CircleAvatar(child: Text(listViewData[i].avatarURL)),
+            title: new Text(listViewData[i].title),
+            onTap: (){},
+            onLongPress: (){
+              print(
+                Text("Long Pressed"),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class ListViewExample extends StatelessWidget {
   @override
--- demo/lib/model.dart
+++ demo/lib/model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 
 class ListViewModel{
   final String title;
@@ -69,30 +68,3 @@ List listViewData = [
 
 ];
 
-class DisplayListView extends StatefulWidget {
-  @override
-  _DisplayListViewState createState() => _DisplayListViewState();
-}
-
-class _DisplayListViewState extends State {
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder( 
-      itemCount: listViewData.length,
-      itemBuilder: (context, int i) => Column(
-        children: [
-          new ListTile(
-            leading: new CircleAvatar(child: Text(listViewData[i].avatarURL)),
-            title: new Text(listViewData[i].title),
-            onTap: (){},
-            onLongPress: (){
-              print(
-                Text("Long Pressed"),
-              );
-            },
-          ),
-        ],
-      ),
-    );
-  }
-}
